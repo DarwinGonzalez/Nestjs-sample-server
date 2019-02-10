83f22dfcccdca75a6dc3a7db0324646c58f11530
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-02-10 17:05:51 +0100

    Implementing base service to all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { MongooseModule} from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, MongooseModule.forRoot(ConfigurationService.conectionString)],
   controllers: [AppController],
   providers: [AppService],
 })
