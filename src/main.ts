1947775e271479b2b8e672a2a4a840e4fd7801e2
DarwinGonzalez <alu0100812794@ull.edu.es>
2019-02-18 10:03:39 +0100

    Configure Cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useStaticAssets(join(__dirname, '../../public'));
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    exposedHeaders: 'Authorization',
+    methods: ['GET'],
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
